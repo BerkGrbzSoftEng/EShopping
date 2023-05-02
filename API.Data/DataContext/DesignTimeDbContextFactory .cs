f5cc511be124e36b1af5499ec70d036764012efd
Anıl Berk Gürbüz <51217672+BerkGrbzSoftEng@users.noreply.github.com>
2023-05-02 23:46:06 +0300

    Add project files.

---
+++ API.Data/DataContext/DesignTimeDbContextFactory .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Data.DataContext
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<EShopContext>
+    {
+        public EShopContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<EShopContext>();
+            var connectionString = "Server=DESKTOP-TCV6EUK;Database=EShopper;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;";
+            builder.UseSqlServer(connectionString);
+            return new EShopContext(builder.Options);
+        }
+    }
+}
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly EShopContext _context;
+
+        public ProductsController(EShopContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = _context.Products.ToListAsync();
+            return await data;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var data = _context.Products.FindAsync(id);
+            return await data;
+        }
+    }
+}
